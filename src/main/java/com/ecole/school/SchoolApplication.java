581ff5ac0bab2b4285a19c4421a9723e970cb266
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2020-12-15 00:45:23 +0000

    config ok

--- src/main/java/com/ecole/school/SchoolApplication.java
+++ src/main/java/com/ecole/school/SchoolApplication.java
@@ -2,6 +2,14 @@ package com.ecole.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @SpringBootApplication
 public class SchoolApplication {
@@ -9,5 +17,18 @@ public class SchoolApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
+
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list of allowed origins
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "authorization", "x-auth-token"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 }
